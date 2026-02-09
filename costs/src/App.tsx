82bea9503681312b0c2965bc7753bd90a9175d63
bisaio <129079375+bisaio@users.noreply.github.com>
2026-02-09 15:59:33 +0000

    aula_19 - navbar e footer

--- costs/src/App.tsx
+++ costs/src/App.tsx
@@ -1,31 +1,30 @@
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Container from './components/layout/Container';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
 
-      <ul>
-        <Link to='/'>Home</Link>
-        <Link to='/newproject'>New Project</Link>
-        <Link to='/company'>Company</Link>
-        <Link to='/contact'>Contact</Link>
-      </ul>
+      <Navbar />
 
       <Container customClass='min-height'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/newproject' element={<NewProject />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path='/company' element={<Company />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
 
     </Router>
   );
