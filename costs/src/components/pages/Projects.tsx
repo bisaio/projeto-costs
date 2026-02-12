e7cd861a3bf8c5a6956ebbb7936e55e628bee152
bisaio <129079375+bisaio@users.noreply.github.com>
2026-02-12 18:21:58 +0000

    aula_26 - css do Projects

--- costs/src/components/pages/Projects.tsx
+++ costs/src/components/pages/Projects.tsx
@@ -1,6 +1,8 @@
 import { useLocation } from "react-router-dom";
 import LinkButton from "../layout/LinkButton";
 import Message from "../layout/Message";
+import styles from './Projects.module.css'
+import Container from "../layout/Container";
 
 export default function Projects() {
     const location = useLocation();
@@ -8,10 +10,15 @@ export default function Projects() {
     if (location.state) message = location.state.message
 
     return (
-        <div>
-            <h1>My projects</h1>
+        <div className={styles.container}>
+            <div className={styles.title_container}>
+                <h1>My projects</h1>
+                <LinkButton link_to='/newproject' text='Create New Project' />
+            </div>
             {message && <Message type="success" text={message} />}
-            <LinkButton link_to='/newproject' text='Create New Project' />
+            <Container customClass="start">
+                <p>projetos</p>
+            </Container>
         </div>
     )
 }
